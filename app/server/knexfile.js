b2a8f229fcb87100f338e7cb089e73cdff8aa6ad
Ndubuishi Ulenu <nduulenu@gmail.com>
2024-11-07 00:48:57 +0100

    Update knexfile

--- app/server/knexfile.js
+++ app/server/knexfile.js
@@ -1,12 +1,21 @@
+const dotenv= require("dotenv") 
+dotenv.config()
 
 module.exports = {
   development: {
     client: 'mysql2',
     connection: {
-      host: 'localhost',
-      user: 'root',
-      password: '0967',
-      database: 'democredit',
+      // host: 'localhost',
+      // user: 'root',
+      // password: '0967',
+      // database: 'democredit',
+      host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+      port: Number(process.env.DB_PORT) || 3306,
+      ssl: process.env.NODE_ENV === 'production' ? {rejectUnauthorized: true} : false
+
     },
     migrations: {
       directory: './migrations',
