3b6b5a6668cc3fe7660a56c8a7e98498f1405f40
Ndubuishi Ulenu <nduulenu@gmail.com>
2024-11-07 12:37:57 +0100

    Updated express.json middleware position

--- app/server/src/index.ts
+++ app/server/src/index.ts
@@ -15,9 +15,9 @@ const app = express();
 const PORT = process.env.PORT || 9090;
 
 // Middleware
-app.use(cors());  
-app.use(morgan('dev'));  
+app.use(cors());
 app.use(express.json()); 
+app.use(morgan('dev'));  
 app.use(express.urlencoded({ extended: true })); 
 
 
